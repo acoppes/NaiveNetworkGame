b7937bbcda441ba449942722f6c05817cc700b06
Ariel Coppes <ariel.coppes@gmail.com>
2020-08-27 22:58:16 -0300

    just some comments

--- deploy_clients.sh
+++ deploy_clients.sh
@@ -3,8 +3,11 @@
 export SERVER=gemserk.com
 export USER=acoppes
 
+echo 'Creating zip for build'
 cd Client/Builds/Windows
 rm ../NaiveNetworkGame-Windows.zip
 zip -r ../NaiveNetworkGame-Windows.zip .
 cd -
+
+echo 'Uploading build to server'
 scp Client/Builds/NaiveNetworkGame-Windows.zip $USER@$SERVER:NaiveNetworkGame/
