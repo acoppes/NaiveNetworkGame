713fc1bfdc0d00a73812c0c278b06d9ff67c7d0e
Ariel Coppes <ariel.coppes@gmail.com>
2020-09-19 18:55:12 -0300

    command line log file fr server linux

--- build-server-linux.sh
+++ build-server-linux.sh
@@ -3,8 +3,7 @@
 # UNITY_EXECUTABLE_PATH="/mnt/c/UnityHub/2020.2.0a19/Editor/Unity.exe"
 UNITY_EXECUTABLE_PATH="/mnt/c/UnityHub/2020.2.0b2/Editor/Unity.exe"
 
-# -logfile Server/Logs/build-server-linux.log
-BUILD_COMMAND="${UNITY_EXECUTABLE_PATH} -buildTarget Linux64 -forgetProjectPath -projectPath Server -quit -silent-crashes -batchmode -nographics -executeMethod BuildScript.BuildLinuxServer"
+BUILD_COMMAND="${UNITY_EXECUTABLE_PATH} -buildTarget Linux64 -forgetProjectPath -projectPath Server -logfile Logs/linux-build.log -quit -silent-crashes -batchmode -nographics -executeMethod BuildScript.BuildLinuxServer"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
