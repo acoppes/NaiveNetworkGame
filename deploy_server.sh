33c924fb6d0649229f29813cf631bf5104447e8f
Ariel Coppes <ariel.coppes@gmail.com>
2020-09-19 19:01:05 -0300

    rsync with progress bar

--- deploy_server.sh
+++ deploy_server.sh
@@ -6,7 +6,7 @@ export SERVER_IP=209.151.153.172
 # ssh acoppes@$SERVER_IP 'nohup ./stop_server.sh > /dev/null 2>&1 &'
 
 # copy everything to server
-rsync -avz Server/Builds/Linux/ acoppes@$SERVER_IP:Linux
+rsync -avz --progress Server/Builds/Linux/ acoppes@$SERVER_IP:Linux
 
 # ssh restart remote server
 # ssh acoppes@$SERVER_IP 'nohup ./start_server.sh > /dev/null 2>&1 &'
