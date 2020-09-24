2cd5c35eb8245154032b58c84c93a85ae0b909ea
Ariel Coppes <ariel.coppes@gmail.com>
2020-09-23 23:08:23 -0300

    update user interface works without singleton

--- Client/Assets/Scripts/NaiveNetworkGame/Client/Systems/UpdateUserInterfaceSystem.cs
+++ Client/Assets/Scripts/NaiveNetworkGame/Client/Systems/UpdateUserInterfaceSystem.cs
@@ -5,34 +5,26 @@ namespace NaiveNetworkGame.Client.Systems
 {
     public class UpdateUserInterfaceSystem : ComponentSystem
     {
-        protected override void OnCreate()
-        {
-            base.OnCreate();
-            RequireSingletonForUpdate<PlayerController>();
-        }
-
         protected override void OnUpdate()
         {
             // get ui from shared component, singleton or not...
-
-            var playerEntity = GetSingletonEntity<PlayerController>();
-            var player = EntityManager.GetComponentData<PlayerController>(playerEntity);
-            var ui = EntityManager.GetSharedComponentData<UserInterfaceComponent>(playerEntity);
             
-            
-            if (ui.goldLabel != null)
-                ui.goldLabel.SetNumber(player.gold);
-
-            if (ui.playerStats)
+            Entities.ForEach(delegate(Entity entity, UserInterfaceComponent ui, ref PlayerController player)
             {
-                ui.playerStats.currentUnits = player.currentUnits;
-                ui.playerStats.maxUnits = player.maxUnits;
-                ui.playerStats.unitType = player.unitType;
-            }
-            
-            if (ui.spawnUnitButton != null)
-                ui.spawnUnitButton.unitType = player.unitType;
+                if (ui.goldLabel != null)
+                    ui.goldLabel.SetNumber(player.gold);
 
+                if (ui.playerStats)
+                {
+                    ui.playerStats.currentUnits = player.currentUnits;
+                    ui.playerStats.maxUnits = player.maxUnits;
+                    ui.playerStats.unitType = player.unitType;
+                }
+            
+                if (ui.spawnUnitButton != null)
+                    ui.spawnUnitButton.unitType = player.unitType;
+            });
+     
             // set the ui.gold from player gold...
 
             // update the player input state given the ui state too?
