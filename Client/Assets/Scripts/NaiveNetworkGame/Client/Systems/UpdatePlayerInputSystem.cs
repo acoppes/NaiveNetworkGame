d2e30af89e6eed8e7fef367b0ec000200d11e9a8
Ariel Coppes <ariel.coppes@gmail.com>
2020-09-23 23:05:52 -0300

    no need for player input to be singleton

--- Client/Assets/Scripts/NaiveNetworkGame/Client/Systems/UpdatePlayerInputSystem.cs
+++ Client/Assets/Scripts/NaiveNetworkGame/Client/Systems/UpdatePlayerInputSystem.cs
@@ -7,26 +7,17 @@ namespace NaiveNetworkGame.Client.Systems
     // Given mouse state, updates player input state.
     public class UpdatePlayerInputSystem : ComponentSystem
     {
-        protected override void OnCreate()
-        {
-            base.OnCreate();
-            RequireSingletonForUpdate<PlayerInputState>();
-        }
-
         protected override void OnUpdate()
         {
-            var playerInputStateEntity = GetSingletonEntity<PlayerInputState>();
-            var playerInputState = EntityManager.GetComponentData<PlayerInputState>(playerInputStateEntity);
-         
-            playerInputState.selectUnitButtonPressed = Input.GetMouseButtonUp(0);
-            playerInputState.actionButtonPressed = Input.GetMouseButtonUp(1);
-            
             var mousePosition = Input.mousePosition;
             var worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-            playerInputState.position = new float2(worldPosition.x, worldPosition.y);
-
-            SetSingleton(playerInputState);
+            
+            Entities.ForEach(delegate(ref PlayerInputState playerInputState)
+            {
+                playerInputState.selectUnitButtonPressed = Input.GetMouseButtonUp(0);
+                playerInputState.actionButtonPressed = Input.GetMouseButtonUp(1);
+                playerInputState.position = new float2(worldPosition.x, worldPosition.y);
+            });
         }
     }
 }
