4281140e1c38e8562d3c08b5cda6fb39ab9de1f8
Ariel Coppes <ariel.coppes@gmail.com>
2020-10-06 23:25:47 -0300

    added shortcuts to load/unload server scene and second player additive scene

--- Client/Assets/Scripts/Editor/CustomServerMenu.cs
+++ Client/Assets/Scripts/Editor/CustomServerMenu.cs
@@ -1,5 +1,8 @@
-
+using System.Linq;
 using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
 
 namespace Editor
 {
@@ -21,6 +24,43 @@ namespace Editor
             var currentValue = EditorPrefs.GetBool(kUseRemoteServerEditorPref, false);
             Menu.SetChecked(kUseRemoteServerByDefault, currentValue);
             return true;
+        }
+        
+        [MenuItem("NaiveNetworkGame/Toggle Local Server")]
+        [Shortcut("NaiveNetworkGame/Toggle Local Server", KeyCode.T, 
+            ShortcutModifiers.Shift | ShortcutModifiers.Alt)]
+        public static void ToggleServerScene()
+        {
+            ToggleScene("ServerScene");
+        }
+        
+        [MenuItem("NaiveNetworkGame/Toggle Second Player")]
+        [Shortcut("NaiveNetworkGame/Toggle Second Player", KeyCode.Y, 
+            ShortcutModifiers.Shift | ShortcutModifiers.Alt)]
+        public static void ToggleSecondPlayer()
+        {
+            ToggleScene("ClientSceneTwoPlayers");
+        }
+
+        private static void ToggleScene(string serverSceneName)
+        {
+            for (var i = 0; i < EditorSceneManager.sceneCount; i++)
+            {
+                var scene = EditorSceneManager.GetSceneAt(i);
+                if (scene.name.Equals(serverSceneName))
+                {
+                    EditorSceneManager.UnloadSceneAsync(scene);
+                    return;
+                }
+            }
+                
+            var guids = AssetDatabase.FindAssets($"t:scene {serverSceneName}");
+            var serverScenePath = guids.Where(g =>
+                AssetDatabase.LoadAssetAtPath<SceneAsset>(AssetDatabase.GUIDToAssetPath(g)).name
+                    .Equals(serverSceneName)).Select(g => AssetDatabase.GUIDToAssetPath(g)).First();
+                
+            var newServerScene = EditorSceneManager.OpenScene(serverScenePath, OpenSceneMode.Additive);
+            EditorSceneManager.SetActiveScene(newServerScene);
         }
     }
 }
