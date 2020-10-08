48e87b8fc251482d2a38ecab7d9a5b4320230030
Ariel Coppes <ariel.coppes@gmail.com>
2020-10-08 00:09:42 -0300

    testing disabing system autocreation and addnig it for a test case

--- Client/Assets/Scenes/Tests/Attack/TestUnitsAttackController.cs
+++ Client/Assets/Scenes/Tests/Attack/TestUnitsAttackController.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using Client;
 using NaiveNetworkGame.Client;
-using Unity.Collections;
+using NaiveNetworkGame.Client.Components;
+using NaiveNetworkGame.Common;
 using Unity.Entities;
 using UnityEngine;
 
@@ -9,6 +10,49 @@ namespace Scenes.Tests
 {
     public class TestUnitsAttackController : MonoBehaviour
     {
+        [DisableAutoCreation]
+        private class TestUnitsAttackComponentSystem : ComponentSystem
+        {
+            private bool done;
+            
+            protected override void OnUpdate()
+            {
+                Entities.WithAll<LocalPlayerController, NetworkPlayerId>()
+                    .ForEach(delegate(ref LocalPlayerController p)
+                    {
+                        var command = PostUpdateCommands.CreateEntity();
+                        PostUpdateCommands.AddComponent(command, new ClientPlayerAction
+                        {
+                            player = p.player,
+                            unit = 0,
+                            actionType = 2,
+                            unitType = 1
+                        });
+
+                        done = true;
+                    });
+
+                // PostUpdateCommands.AddComponent(PostUpdateCommands.CreateEntity(), new ClientPlayerAction
+                // {
+                //     player = 1,
+                //     unit = 0,
+                //     actionType = 2,
+                //     unitType = 1
+                // });
+                //
+                // // wait some time
+                //
+                // PostUpdateCommands.AddComponent(PostUpdateCommands.CreateEntity(), new ClientPlayerAction
+                // {
+                //     player = 1,
+                //     unit = 0,
+                //     actionType = 2,
+                //     unitType = 0
+                // });
+                
+            }
+        }
+        
         public int unitsCount = 1;
         
         // Start is called before the first frame update
@@ -19,38 +63,55 @@ namespace Scenes.Tests
 
         private IEnumerator StartTest()
         {
-            var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            // var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+            var world = World.DefaultGameObjectInjectionWorld;
 
-            yield return new WaitUntil(() => ConnectionState.currentState == ConnectionState.State.Connected);
+            var testUnitsAttack = world.GetOrCreateSystem<TestUnitsAttackComponentSystem>();
             
-            yield return new WaitForSeconds(0.1f);
+            var simulationSystemGroup = world.GetOrCreateSystem<SimulationSystemGroup>();
+            simulationSystemGroup.AddSystemToUpdateList(testUnitsAttack);
+            
+            // simulationSystemGroup.SortSystemUpdateList();
+            
+            ScriptBehaviourUpdateOrder.UpdatePlayerLoop(world);
+            
+            // world.CreateManager<MyCustomSystem>();
+            // world.CreateManager<MyOtherCustomSystem>();
+            // ScriptBehaviourUpdateOrder.UpdatePlayerLoop(world);
+            //
+            //
+            // yield return new WaitUntil(() => ConnectionState.currentState == ConnectionState.State.Connected);
+            //
+            // yield return new WaitForSeconds(0.1f);
+            //
+            // var playerInputStateQuery = entityManager.CreateEntityQuery(
+            //     ComponentType.ReadWrite<PlayerPendingAction>());
+            //
+            // while (unitsCount > 0)
+            // {
+            //     var entities = playerInputStateQuery.ToEntityArray(Allocator.TempJob);
+            //     var inputStates = playerInputStateQuery.ToComponentDataArray<PlayerPendingAction>(Allocator.TempJob);
+            //
+            //     for (int i = 0; i < inputStates.Length; i++)
+            //     {
+            //         var inputState = inputStates[i];
+            //         
+            //         inputState.pending = true;
+            //         inputState.actionType = 2;
+            //         inputState.unitType = 0;
+            //
+            //         entityManager.SetComponentData(entities[i], inputState);
+            //     }
+            //
+            //     entities.Dispose();
+            //     inputStates.Dispose();
+            //
+            //     unitsCount--;
+            //     
+            //     yield return null;
+            // }
 
-            var playerInputStateQuery = entityManager.CreateEntityQuery(
-                ComponentType.ReadWrite<PlayerPendingAction>());
-
-            while (unitsCount > 0)
-            {
-                var entities = playerInputStateQuery.ToEntityArray(Allocator.TempJob);
-                var inputStates = playerInputStateQuery.ToComponentDataArray<PlayerPendingAction>(Allocator.TempJob);
-
-                for (int i = 0; i < inputStates.Length; i++)
-                {
-                    var inputState = inputStates[i];
-                    
-                    inputState.pending = true;
-                    inputState.actionType = 2;
-                    inputState.unitType = 0;
-
-                    entityManager.SetComponentData(entities[i], inputState);
-                }
-
-                entities.Dispose();
-                inputStates.Dispose();
-
-                unitsCount--;
-                
-                yield return null;
-            }
+            yield return null;
         }
 
     }
