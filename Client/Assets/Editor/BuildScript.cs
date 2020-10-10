35f5b4f7e0ade28dd8c3f2aeb30bd1a32f76cde5
Ariel Coppes <ariel.coppes@gmail.com>
2020-10-10 18:53:12 -0300

    android keystore env vars for build

--- Client/Assets/Editor/BuildScript.cs
+++ Client/Assets/Editor/BuildScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
@@ -6,6 +7,16 @@ public class BuildScript : MonoBehaviour
 {
     public static void BuildAndroidPlayer()
     {
+        var keystorePath = Environment.GetEnvironmentVariable("BUILD_KEY_STORE_PATH");
+        if (!string.IsNullOrWhiteSpace(keystorePath))
+        {
+            PlayerSettings.Android.useCustomKeystore = true;
+            PlayerSettings.Android.keystoreName = keystorePath;
+            PlayerSettings.Android.keyaliasName = Environment.GetEnvironmentVariable("BUILD_KEY_STORE_ALIAS");
+            PlayerSettings.Android.keystorePass = Environment.GetEnvironmentVariable("BUILD_KEY_STORE_PASS");
+            PlayerSettings.Android.keyaliasPass = Environment.GetEnvironmentVariable("BUILD_KEY_STORE_ALIAS_PASS");
+        }
+
         var playerOptions = new BuildPlayerOptions
         {
             target = BuildTarget.Android,
