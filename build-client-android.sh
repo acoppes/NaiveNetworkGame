6ec324749d9848a26d16ff5e39744bee1b733949
Ariel Coppes <ariel.coppes@gmail.com>
2020-10-10 18:56:22 -0300

    log for android build

--- build-client-android.sh
+++ build-client-android.sh
@@ -3,7 +3,7 @@
 UNITY_EXECUTABLE_PATH="/mnt/c/UnityHub/2020.2.0b2/Editor/Unity.exe"
 
 # -logfile Client/Logs/build-client-windows.log
-BUILD_COMMAND="${UNITY_EXECUTABLE_PATH} -buildTarget Android -forgetProjectPath -projectPath Client -quit -silent-crashes -batchmode -nographics -executeMethod BuildScript.BuildAndroidPlayer"
+BUILD_COMMAND="${UNITY_EXECUTABLE_PATH} -buildTarget Android -forgetProjectPath -projectPath Client -logfile Client/Logs/android-build.log -quit -silent-crashes -batchmode -nographics -executeMethod BuildScript.BuildAndroidPlayer"
 
 echo ${BUILD_COMMAND}
 ${BUILD_COMMAND}
