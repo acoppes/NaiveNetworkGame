a9d9d6386fef3ca0646e69e011be670ae7a39952
Ariel Coppes <ariel.coppes@gmail.com>
2020-10-09 21:58:52 -0300

    adding more logic to custom authoring

--- Server/Assets/NaiveNetworkGame.Server/Components/UnitCustomAuthoring.cs
+++ Server/Assets/NaiveNetworkGame.Server/Components/UnitCustomAuthoring.cs
@@ -1,3 +1,4 @@
+using NaiveNetworkGame.Common;
 using Unity.Entities;
 using UnityEngine;
 
@@ -5,10 +6,37 @@ namespace NaiveNetworkGame.Server.Components
 {
     public class UnitCustomAuthoring : MonoBehaviour, IConvertGameObjectToEntity
     {
+        // public float damage;
+        // public float range;
+        public byte player;
+        public float speed;
+        public float health;
         public float spawnDuration = 0;
         
         public void Convert(Entity entity, EntityManager entityManager, GameObjectConversionSystem conversionSystem)
         {
+            entityManager.AddComponentData(entity, new Unit
+            {
+                player = player
+            });
+            
+            entityManager.AddComponentData(entity, new Movement
+            {
+                speed = speed
+            });
+            entityManager.AddComponentData(entity, new UnitState());
+            // entityManager.AddComponentData(entity, new Attack
+            // {
+            //     damage = damage,
+            //     range = range
+            // });
+            entityManager.AddComponentData(entity, new LookingDirection());
+            entityManager.AddComponentData(entity, new Health
+            {
+                total = health,
+                current = health
+            });
+
             entityManager.AddComponentData(entity, new IsAlive());
             entityManager.AddComponentData(entity, new UnitBehaviour());
 
