fe8452b3c941c3f9d02f81e34525594334732e06
Ariel Coppes <ariel.coppes@gmail.com>
2020-10-16 22:18:11 -0300

    testing copy start/stop sh with server upload

--- Server/start_server.sh
+++ Server/start_server.sh
@@ -4,6 +4,11 @@ echo "killing previous server instance"
 pkill -9 server.x86_64
 
 # wait $!
-./Linux/server.x86_64 -targetFrameRate 60 -logStatistics > server_output.txt &
+./Linux/server.x86_64 -port 9000 -logStatistics > output_server1.txt &
+./Linux/server.x86_64 -port 9001 -logStatistics > output_server2.txt &
+./Linux/server.x86_64 -port 9002 -logStatistics > output_server3.txt &
+./Linux/server.x86_64 -port 9003 -logStatistics > output_server4.txt &
+./Linux/server.x86_64 -port 9004 -logStatistics > output_server5.txt &
+
 echo "new server instance created"
 
--- deploy_server.sh
+++ deploy_server.sh
@@ -6,6 +6,9 @@ export SERVER_IP=209.151.153.172
 # ssh acoppes@$SERVER_IP 'nohup ./stop_server.sh > /dev/null 2>&1 &'
 
 # copy everything to server
+cp Server/start_server.sh Server/Builds/Linux
+cp Server/stop_server.sh Server/Builds/Linux
+
 rsync -avz --progress Server/Builds/Linux/ acoppes@$SERVER_IP:Linux
 
 # ssh restart remote server
