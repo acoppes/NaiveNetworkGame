c71db2de4c202f7e23bda321130a09e84d92fcbc
Ariel Coppes <ariel.coppes@gmail.com>
2020-10-25 14:00:36 -0300

    new unity version

--- build-client-android.sh
+++ build-client-android.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-UNITY_EXECUTABLE_PATH="/mnt/c/UnityHub/2020.2.0b2/Editor/Unity.exe"
+UNITY_EXECUTABLE_PATH="/mnt/c/UnityHub/2020.2.0b8/Editor/Unity.exe"
 
 # -logfile Client/Logs/build-client-windows.log
 BUILD_COMMAND="${UNITY_EXECUTABLE_PATH} -buildTarget Android -forgetProjectPath -projectPath Client -logfile Client/Logs/android-build.log -quit -silent-crashes -batchmode -nographics -executeMethod BuildScript.BuildAndroidPlayer"
--- build-server-linux.sh
+++ build-server-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # UNITY_EXECUTABLE_PATH="/mnt/c/UnityHub/2020.2.0a19/Editor/Unity.exe"
-UNITY_EXECUTABLE_PATH="/mnt/c/UnityHub/2020.2.0b2/Editor/Unity.exe"
+UNITY_EXECUTABLE_PATH="/mnt/c/UnityHub/2020.2.0b8/Editor/Unity.exe"
 
 BUILD_COMMAND="${UNITY_EXECUTABLE_PATH} -buildTarget Linux64 -forgetProjectPath -projectPath Server -logfile Server/Logs/linux-build.log -quit -silent-crashes -batchmode -nographics -executeMethod BuildScript.BuildLinuxServer"
 
