ccbb57799b21342e9549e64931cdc7a0bcd9bb46
Ariel Coppes <ariel.coppes@gmail.com>
2026-02-10 10:58:27 -0300

    first autoconversion of code testing ai agent

--- Client/Assets/Scripts/NaiveNetworkGame/Client/Systems/CreateInterpolationFromTranslationSync.cs
+++ Client/Assets/Scripts/NaiveNetworkGame/Client/Systems/CreateInterpolationFromTranslationSync.cs
@@ -1,3 +1,51 @@
+// using NaiveNetworkGame.Common;
+// using Unity.Entities;
+// using Unity.Transforms;
+//
+// namespace NaiveNetworkGame.Client.Systems
+// {
+//     [UpdateAfter(typeof(CopyTranslationSyncToUnit))]
+//     public partial class CreateInterpolationFromTranslationSync : SystemBase
+//     {
+//         protected override void OnUpdate()
+//         {
+//             // TODO: we might add the translation component too here...
+//             
+//             // first time interpolation created with proper translation...
+//             Entities
+//                 .WithNone<TranslationInterpolation>()
+//                 .WithAll<Unit, Translation, NetworkTranslationSync, ClientOnly>()
+//                 .ForEach(delegate(Entity e, ref Translation t, ref NetworkTranslationSync n)
+//                 {
+//                     // interpolation component was created with unit the first time...
+//                     
+//                     PostUpdateCommands.AddComponent(e, new TranslationInterpolation
+//                     {
+//                         previousTranslation = n.translation,
+//                         currentTranslation = n.translation,
+//                         time = 0,
+//                         remoteDelta = n.delta
+//                     });
+//                 });
+//             
+//             Entities
+//                 .WithAll<Unit, Translation, NetworkTranslationSync, ClientOnly>()
+//                 .ForEach(delegate(Entity e, ref Translation t, ref NetworkTranslationSync n,
+//                     ref TranslationInterpolation interpolation)
+//                 {
+//                     // interpolation component was created with unit the first time...
+//             
+//                     interpolation.previousTranslation = t.Value.xy;
+//                     interpolation.currentTranslation = n.translation;
+//                     interpolation.remoteDelta = n.delta;
+//                     interpolation.time = 0;
+//                     
+//                     PostUpdateCommands.RemoveComponent<NetworkTranslationSync>(e);
+//                 });
+//         }
+//     }
+// }
+
 using NaiveNetworkGame.Common;
 using Unity.Entities;
 using Unity.Transforms;
@@ -5,43 +53,41 @@ using Unity.Transforms;
 namespace NaiveNetworkGame.Client.Systems
 {
     [UpdateAfter(typeof(CopyTranslationSyncToUnit))]
-    public partial class CreateInterpolationFromTranslationSync : SystemBase
+    public partial struct CreateInterpolationFromTranslationSync : ISystem
     {
-        protected override void OnUpdate()
+        public void OnUpdate(ref SystemState state)
         {
-            // TODO: we might add the translation component too here...
-            
-            // first time interpolation created with proper translation...
-            Entities
-                .WithNone<TranslationInterpolation>()
-                .WithAll<Unit, Translation, NetworkTranslationSync, ClientOnly>()
-                .ForEach(delegate(Entity e, ref Translation t, ref NetworkTranslationSync n)
+            // First time interpolation created with proper translation...
+            foreach (var (_, networkSync, entity) in 
+                     SystemAPI.Query<RefRO<LocalTransform>, RefRO<NetworkTranslationSync>>()
+                         .WithNone<TranslationInterpolation>()
+                         .WithAll<Unit, ClientOnly>()
+                         .WithEntityAccess())
+            {
+                state.EntityManager.AddComponentData(entity, new TranslationInterpolation
                 {
-                    // interpolation component was created with unit the first time...
-                    
-                    PostUpdateCommands.AddComponent(e, new TranslationInterpolation
-                    {
-                        previousTranslation = n.translation,
-                        currentTranslation = n.translation,
-                        time = 0,
-                        remoteDelta = n.delta
-                    });
+                    previousTranslation = networkSync.ValueRO.translation,
+                    currentTranslation = networkSync.ValueRO.translation,
+                    time = 0,
+                    remoteDelta = networkSync.ValueRO.delta
                 });
+            }
             
-            Entities
-                .WithAll<Unit, Translation, NetworkTranslationSync, ClientOnly>()
-                .ForEach(delegate(Entity e, ref Translation t, ref NetworkTranslationSync n,
-                    ref TranslationInterpolation interpolation)
-                {
-                    // interpolation component was created with unit the first time...
-            
-                    interpolation.previousTranslation = t.Value.xy;
-                    interpolation.currentTranslation = n.translation;
-                    interpolation.remoteDelta = n.delta;
-                    interpolation.time = 0;
-                    
-                    PostUpdateCommands.RemoveComponent<NetworkTranslationSync>(e);
-                });
+            // Update existing interpolation components
+            foreach (var (localTransform, networkSync, interpolationRW, entity) in 
+                     SystemAPI.Query<RefRO<LocalTransform>, RefRO<NetworkTranslationSync>, RefRW<TranslationInterpolation>>()
+                         .WithAll<Unit, ClientOnly>()
+                         .WithEntityAccess())
+            {
+                ref var interpolation = ref interpolationRW.ValueRW;
+                
+                interpolation.previousTranslation = localTransform.ValueRO.Position.xy;
+                interpolation.currentTranslation = networkSync.ValueRO.translation;
+                interpolation.remoteDelta = networkSync.ValueRO.delta;
+                interpolation.time = 0;
+                
+                state.EntityManager.RemoveComponent<NetworkTranslationSync>(entity);
+            }
         }
     }
 }
